8a050189a3cf1fdcb1c8dba944b79aa54b03e1e4
KHSolloway <KHSolloway@my.waketech.edu>
2025-04-30 14:54:24 -0400

    updated to gradebook project

--- public/gradebook.js
+++ public/gradebook.js
@@ -6,16 +6,14 @@ function fetchGradeData() {
         let results;
         if(xhr.readyState === xhr.DONE){
             if(xhr.status !== 200){
-                console.error("Could not get grades. Status: ${xhr.status}");
+                console.error(`Could not get grades. Status: ${xhr.status}`);
     }
     populateGradebook(JSON.parse(xhr.responseText));
     }
-
 }.bind(this);
 xhr.open("get", apiRoute, true);
 xhr.send();
 }
-
 
 function populateGradebook(data){
     console.log("Populating the gradebook with data:", data);
@@ -23,11 +21,11 @@ function populateGradebook(data){
         data.forEach(function(assignment){
             let row = document.createElement("tr");
             let columns = [];
-            columns.name = document.createElement("td");
+            columns.name = document.createElement('td');
             columns.name.appendChild(
                 document.createTextNode(assignment.last_name + ", "+ assignment.first_name)
             );
-            columns.grade = document.createElement("td");
+            columns.grade = document.createElement('td');
             columns.grade.appendChild(
                 document.createTextNode(assignment.total_grade)
             );
@@ -38,3 +36,5 @@ function populateGradebook(data){
 
         });
 }
+
+fetchGradeData();
