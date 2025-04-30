89aebcbaab481220342e7239b54baec1f3c669bd
KHSolloway <KHSolloway@my.waketech.edu>
2025-04-30 11:39:19 -0400

    update gradebook files

--- cti_100_node.js
+++ cti_100_node.js
@@ -32,7 +32,7 @@ router.get('/api/grades',function(req, res){
     pool.query(
         `SELECT Students.student_id, first_name, last_name, AVG(assignments.grade) as total_grade \
             FROM Students  \
-            LEFT JOIN Assignments ON Assignments.student_id = Students.student_id \
+            LEFT JOIN assignments ON assignments.student_id = Students.student_id \
             GROUP BY Students.student_id \
             ORDER BY total_grade DESC`,
         [],
